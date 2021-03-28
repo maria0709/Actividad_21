d3d4ea96fc6f345bba37fe922e73c4fd5466436e
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-28 01:23:31 -0300

    Entrega de la actividad 21, a pedido del docente.

---
+++ Actividad_21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_21
+{ class Program
+    {     
+    //Presente al usuario la fecha actual.
+    static void Main(string[] args)
+        {
+        DateTime datenow = DateTime.Now;
+
+        Console.WriteLine("La fecha de hoy es: " + datenow);
+        Console.ReadKey();
+        }
+    }
+}
